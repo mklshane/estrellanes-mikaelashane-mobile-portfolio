0900d101fb6a07310d8900d1d92459df6f5b5e4a
Mikaela Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2026-01-19 20:46:17 +0800

    feat: profile section with modular components

---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,41 @@
+import { View, Text, Dimensions } from "react-native";
+import React from "react";
+import { styles } from "@styles/globalStyles";
+import ProfileCard from "./ProfileCard";
+import { useTheme } from "@context/ThemeContext";
+
+const screenHeight = Dimensions.get("window").height - 170;
+
+const Profile: React.FC = () => {
+  const { colors } = useTheme();
+
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor: colors.background[0],
+          width: "100%",
+          height: screenHeight, 
+          paddingHorizontal: 35,
+        },
+      ]}
+    >
+      <Text
+        style={{
+          alignSelf: "center",
+          fontSize: 100,
+          fontWeight: "bold",
+          color: colors.text,
+          fontFamily: "PinyonScript",
+        }}
+      >
+        Portfolio
+      </Text>
+
+      <ProfileCard />
+    </View>
+  );
+};
+
+export default Profile;
